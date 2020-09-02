56283863604bb0cd2cb8b7f71df75f3f6d679817
kryptand <goetze-a@outlook.com>
2020-09-02 21:01:06 +0200

    feat(core): initialize projects and dependencies

---
+++ libs/web/image-upload/stencil.config.ts
@@ -0,0 +1,21 @@
+import { Config } from '@stencil/core';
+
+export const config: Config = {
+  namespace: 'web-image-upload',
+  taskQueue: 'async',
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+      dir: '../../dist/libs/web-image-upload/dist',
+    },
+    {
+      type: 'docs-readme',
+    },
+    {
+      type: 'www',
+      dir: '../../dist/libs/web-image-upload/www',
+      serviceWorker: null, // disable service workers
+    },
+  ],
+};
